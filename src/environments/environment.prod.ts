c04e2a2328feafb11ead22599450790116b5a500
Evgeny <georgem71@gmail.com>
2019-09-27 22:38:20 +0300

    Change Environment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://dataservice.accuweather.com',
+  appKey: 'hjZuRkwFx2UcPWrPg2b6oRK4DoqsUKlG',
+  // appKey: 'HAwqN4nGaPNIPr19Zf0WwxZi7dq2WAsv',
+  defaultCurrentLocation: 'Tel Aviv' // 215854
 };
