ab381d2d798c6bc5af9ba1ae27f1ae7416766dec
Evgeny <georgem71@gmail.com>
2019-09-28 19:17:07 +0300

    Content Security Policy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://dataservice.accuweather.com',
+  apiUrl: 'https://dataservice.accuweather.com',
   appKey: 'hjZuRkwFx2UcPWrPg2b6oRK4DoqsUKlG',
   // appKey: 'HAwqN4nGaPNIPr19Zf0WwxZi7dq2WAsv',
   defaultCurrentLocation: 'Tel Aviv' // 215854
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://dataservice.accuweather.com',
+  apiUrl: 'https://dataservice.accuweather.com',
   appKey: 'hjZuRkwFx2UcPWrPg2b6oRK4DoqsUKlG',
   // appKey: 'HAwqN4nGaPNIPr19Zf0WwxZi7dq2WAsv',
   defaultCurrentLocation: 'Tel Aviv' // 215854
