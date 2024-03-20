0dfc64811e06c0a4dc340da251bc671c80fd9dd6
Evgeny Miloslavsky <georgem71@gmail.com>
2024-03-20 11:53:49 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://dataservice.accuweather.com',
-  appKey: 'hjZuRkwFx2UcPWrPg2b6oRK4DoqsUKlG',
-  // appKey: 'HAwqN4nGaPNIPr19Zf0WwxZi7dq2WAsv',
+  appKey: '@@@',
   defaultCurrentLocation: 'Tel Aviv' // 215854
 };
 
